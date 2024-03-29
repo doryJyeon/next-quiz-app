35d2baed7045d2d92fdea41a391adf75ac8de5ef
JYeon <doryJYeon@gmail.com>
2024-03-29 12:38:06 +0900

    [+] quiz app

---
+++ app/state/page.tsx
@@ -0,0 +1,15 @@
+import State from '@/components/State';
+import { getStates } from '@/utils/functions';
+import React from 'react'
+
+const StatePage = () => {
+  const states = getStates();
+
+  return (
+    <div className="flex flex-col space-y-4 md:space-y-8 p-4 lg:p-8">
+      <State states={states} />
+    </div>
+  )
+}
+
+export default StatePage
